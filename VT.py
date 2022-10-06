1391a674136f2d958247381dd49068e7ddc6db1e
Kenkeying <113969322+Kenkeying@users.noreply.github.com>
2022-10-06 10:34:22 +0800

    Create VT.py

---
+++ VT.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+
+
+import torch
+import torch.nn.functional as F
+from einops import rearrange
+from torch import nn
+import torch.nn.init as init
+
+
+def _weights_init(m):
+
+    classname = m.__class__.__name__
+
+    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+        init.kaiming_normal_(m.weight)
+
+
+class LambdaLayer(nn.Module):
+    def __init__(self, lambd):
+        super(LambdaLayer, self).__init__()
+        self.lambd = lambd
+
+    def forward(self, x):
+        return self.lambd(x)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1, option='A'):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+            if option == 'A':
+                """
+                For CIFAR10 ResNet paper uses option A.
+                """
+                self.shortcut = LambdaLayer(lambda x:
+                                            F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, planes//4, planes//4), "constant", 0))
+            elif option == 'B':
+                self.shortcut = nn.Sequential(
+                     nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                     nn.BatchNorm2d(self.expansion * planes)
+                )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+
+        return out
+
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        return self.fn(x, **kwargs) + x
+
+
+class LayerNormalize(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+
+class MLP_Block(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout=0.1):
+        super().__init__()
+        self.nn1 = nn.Linear(dim, hidden_dim)
+        torch.nn.init.xavier_uniform_(self.nn1.weight)
+        torch.nn.init.normal_(self.nn1.bias, std=1e-6)
+        self.af1 = nn.GELU()
+        self.do1 = nn.Dropout(dropout)
+        self.nn2 = nn.Linear(hidden_dim, dim)
+        torch.nn.init.xavier_uniform_(self.nn2.weight)
+        torch.nn.init.normal_(self.nn2.bias, std=1e-6)
+        self.do2 = nn.Dropout(dropout)
+        
+    def forward(self, x):
+        x = self.nn1(x)
+        x = self.af1(x)
+        x = self.do1(x)
+        x = self.nn2(x)
+        x = self.do2(x)
+        
+        return x
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads=8, dropout=0.1):
+        super().__init__()
+        self.heads = heads
+        self.scale = dim ** -0.5
+
+        self.to_qkv = nn.Linear(dim, dim * 3, bias=True)
+        torch.nn.init.xavier_uniform_(self.to_qkv.weight)
+        torch.nn.init.zeros_(self.to_qkv.bias)
+        
+        self.nn1 = nn.Linear(dim, dim)
+        torch.nn.init.xavier_uniform_(self.nn1.weight)
+        torch.nn.init.zeros_(self.nn1.bias)        
+        self.do1 = nn.Dropout(dropout)
+
+    def forward(self, x, mask=None):
+        b, n, _, h = *x.shape, self.heads
+        qkv = self.to_qkv(x)
+        q, k, v = rearrange(qkv, 'b n (qkv h d) -> qkv b h n d', qkv=3, h=h)
+
+        dots = torch.einsum('bhid,bhjd->bhij', q, k) * self.scale
+
+        if mask is not None:
+            mask = F.pad(mask.flatten(1), (1, 0), value = True)
+            assert mask.shape[-1] == dots.shape[-1], 'mask has incorrect dimensions'
+            mask = mask[:, None, :] * mask[:, :, None]
+            dots.masked_fill_(~mask, float('-inf'))
+            del mask
+
+        attn = dots.softmax(dim=-1)
+
+        out = torch.einsum('bhij,bhjd->bhid', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out = self.nn1(out)
+        out = self.do1(out)
+        return out
+
+
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, mlp_dim, dropout):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                Residual(LayerNormalize(dim, Attention(dim, heads=heads, dropout=dropout))),
+                Residual(LayerNormalize(dim, MLP_Block(dim, mlp_dim, dropout=dropout)))
+            ]))
+
+    def forward(self, x, mask=None):
+        for attention, mlp in self.layers:
+            x = attention(x, mask=mask)
+            x = mlp(x)
+        return x
+
